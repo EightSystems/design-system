2c776ed4dd6ac0adae943f6d4815800f843fc871
Ernesto Resende <ernestoresende@hotmail.com>
2021-03-23 23:14:03 -0300

    chore: organizing design system themes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./Button.scss"
 
 const Button = ({ children }) => {
-    return <button className='button button--primary'>{children}</button>
+    return <button className='button button--primary__outline'>{children}</button>
 }
 
 export default Button
