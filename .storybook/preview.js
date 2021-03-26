557c7d8a45e62873ca5fe1f9edad488009db9104
Ernesto Resende <ernestoresende@hotmail.com>
2021-03-26 18:04:51 -0300

    chore: testing storybook stories functionalities

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
-const {addDecorator} = require('@storybook/react')
-const {withPropsTable} = require('storybook-addon-react-docgen')
-const {withA11y} = require('@storybook/addon-a11y');
+const { addDecorator } = require("@storybook/react")
+const { withPropsTable } = require("storybook-addon-react-docgen")
+const { withA11y } = require("@storybook/addon-a11y")
+import centered from "@storybook/addon-centered/react"
 
 addDecorator(withPropsTable)
 addDecorator(withA11y)
+addDecorator(centered)
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,52 @@
 import React from "react"
 import "./Button.scss"
+import Proptypes from "prop-types"
+import classnames from "classnames"
 
-const Button = ({ children }) => {
-    return <button className='button button--primary__outline'>{children}</button>
+const Button = ({
+    children,
+    color,
+    outline,
+    onClick,
+    disabled,
+    ref,
+    className,
+    "aria-label": ariaLabel,
+}) => {
+    const buttonClasses = classnames({
+        "button--primary": color === "primary",
+        "button--danger": color === "danger",
+        "button--primary__outline": color === "primary" && outline === true,
+        "button--danger__outline": color === "danger" && outline === true,
+    })
+
+    return (
+        <button
+            className={`${className ? className + " " : ""}button ${buttonClasses}`}
+            onClick={onClick}
+            disabled={disabled}
+            ref={ref}
+            aria-label={ariaLabel}
+        >
+            {children}
+        </button>
+    )
+}
+
+Button.propTypes = {
+    children: Proptypes.node,
+    color: Proptypes.oneOf(["primary", "danger"]),
+    outline: Proptypes.bool,
+    onClick: Proptypes.func,
+    disabled: Proptypes.bool,
+    ref: Proptypes.oneOf([Proptypes.object, Proptypes.func, Proptypes.string]),
+    className: Proptypes.string,
+    "aria-label": Proptypes.string,
+}
+
+Button.defaultProps = {
+    color: "primary",
+    outline: false,
 }
 
 export default Button
