b448bce04856e892083ac841454a8f6ec717b072
Ernesto Resende <ernestoresende@hotmail.com>
2021-04-06 00:55:27 -0300

    refactor: removed build breaking component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Button from "./components/Button";
 import Avatar from "./components/Avatar";
-import Tooltip from "./components/Tooltip";
 
 export { Button };
 export { Avatar };
-export { Tooltip }
