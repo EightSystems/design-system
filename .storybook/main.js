a2dd2f4190dc94a0dbe1a80bc67000946b7ae9f1
Ernesto Resende <ernestoresende@hotmail.com>
2021-04-06 00:52:47 -0300

    refactor: storybook configuration files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,36 +1,11 @@
-const path = require("path");
-
 module.exports = {
-    stories: ["../src/stories/**/*.stories.(js|jsx|mdx)"],
-    addons: ["@storybook/addon-a11y", "storybook-addon-react-docgen"],
-
-    webpackFinal: async (config, { configType }) => {
-        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-        // You can change the configuration based on that.
-        // 'PRODUCTION' is used when building the static version of storybook.
-
-        config.module.rules.map(rule => {
-            if (rule.oneOf) {
-                rule.oneOf = rule.oneOf.slice().map(subRule => {
-                    if (subRule.test instanceof RegExp && subRule.test.test(".scss")) {
-                        return {
-                            ...subRule,
-                            use: [
-                                ...subRule.use,
-                                {
-                                    loader: require.resolve("sass-resources-loader"),
-                                    options: {
-                                        resources: [path.resolve(__dirname, "../src/styles/_global.scss")],
-                                    },
-                                },
-                            ],
-                        };
-                    }
-                    return subRule;
-                });
-            }
-            return rule;
-        });
-        return config;
-    },
-};
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|mdx)"
+  ],
+  "addons": [
+    "@storybook/addon-docs",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
--- .storybook/preview.js
+++
@@ -1,8 +0,0 @@
-const { addDecorator } = require("@storybook/react");
-const { withPropsTable } = require("storybook-addon-react-docgen");
-const { withA11y } = require("@storybook/addon-a11y");
-import centered from "@storybook/addon-centered/react";
-
-addDecorator(withPropsTable);
-addDecorator(withA11y);
-addDecorator(centered);
