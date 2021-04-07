8be157092448b1b4fc183f5e131eb82d9b39ba89
Ernesto Resende <ernestoresende@hotmail.com>
2021-04-07 18:52:36 -0300

    refactor: button to match styled-components props based syntax (#1)

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,32 +1,78 @@
 import React from "react";
+import styled from "styled-components";
 import Proptypes from "prop-types";
-import classnames from "classnames";
-import * as S from "./styles";
+import { GlobalStyles } from "../../styles/theme";
+import * as V from "../../styles/variables";
+
+const ButtonWrapper = styled.button`
+    ${GlobalStyles};
+    font-family: ${V.FontFaces.Poppins};
+    font-size: 18px;
+    appearance: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: 600;
+    padding: 18px 24px;
+    text-decoration: none;
+    text-align: center;
+    transition: 150ms ease-in-out;
+
+    &[data-color="primary"] {
+        background: var(--primary);
+        border: solid 2px var(--primary);
+        color: var(--primaryContrast);
+
+        &[data-hasOutline="true"] {
+            background: none;
+            border: solid 2px var(--primary);
+            color: var(--primary);
+        }
+        &:hover {
+            background: var(--primaryTint);
+            border: solid 2px var(--primaryTint);
+            color: var(--primaryContrast);
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.35);
+        }
+    }
+
+    &[data-color="danger"] {
+        background: var(--danger);
+        border: solid 2px var(--danger);
+        color: var(--primaryContrast);
+
+        &[data-hasOutline="true"] {
+            background: none;
+            border: solid 2px var(--danger);
+            color: var(--danger);
+        }
+        &:hover {
+            background: var(--dangerTint);
+            border: solid 2px var(--dangerTint);
+            color: var(--primaryContrast);
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.35);
+        }
+    }
+`;
 
 const Button = React.forwardRef((props, componentRef) => {
-    const buttonClasses = classnames({
-        "button--primary": props.color === "primary",
-        "button--danger": props.color === "danger",
-        "button--primary__outline": props.color === "primary" && props.hasOutline === true,
-        "button--danger__outline": props.color === "danger" && props.hasOutline === true,
-    });
+    console.log(props);
     return (
-        <S.Wrapper>
-            <button
-                className={`${props.className ? props.className + " " : ""}button ${buttonClasses}`}
-                type={props.type}
-                onClick={props.onClick}
-                disabled={props.disabled}
-                onMouseEnter={props.onMouseEnter}
-                onMouseLeave={props.onMouseLeave}
-                onFocus={props.onFocus}
-                onBlur={props.onBlur}
-                aria-label={props.ariaLabel}
-                ref={componentRef}
-            >
-                {props.children}
-            </button>
-        </S.Wrapper>
+        <ButtonWrapper
+            className={props.className}
+            data-color={props.color}
+            data-hasOutline={props.hasOutline}
+            type={props.type}
+            onClick={props.onClick}
+            disabled={props.disabled}
+            onMouseEnter={props.onMouseEnter}
+            onMouseLeave={props.onMouseLeave}
+            onFocus={props.onFocus}
+            onBlur={props.onBlur}
+            aria-label={props.ariaLabel}
+            ref={componentRef}
+        >
+            {props.children}
+        </ButtonWrapper>
     );
 });
 
