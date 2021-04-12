01b7d5a554ae2492e43a31d70f92d64058f31e32
Ernesto Resende <ernestoresende@hotmail.com>
2021-04-12 07:19:26 -0300

    chore: reintroduced a11y addon for accessibility checks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|mdx)"],
-    addons: ["@storybook/addon-docs", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+    addons: [
+        "@storybook/addon-docs",
+        "@storybook/addon-a11y", 
+        "@storybook/addon-essentials", 
+        "@storybook/preset-create-react-app"
+    ],
 };
