08d8c7f87b6fdef07c61af565b5ea1003bbb3b27
Vin Souza <vin@8sistemas.com>
2021-04-14 14:30:09 -0300

    Small change to children map and cloneElement props

--- src/components/Stepper/index.js
+++ src/components/Stepper/index.js
@@ -9,39 +9,23 @@ const StepperContainer = styled.div`
     justify-content: space-around;
 `;
 
-const Stepper = React.forwardRef((props, componentRef) => {
-    
-    console.log(props)
-
-    const childrenArray = React.Children.toArray(props.children);
-    const steps = childrenArray.map((step, index) => {
-        const state = {
-            index,
-            active: false,
-        };
-
-        if (props.activeStep === index) {
-            state.active = true;
-        }
-
-        return React.cloneElement(step, {
-            ...state,
-            ...step.props,
-        });
-    });
-
-    console.log(steps)
-
-    return (
+const Stepper = React.forwardRef(
+    ({children, ...props}, componentRef) => (
         <StepperContainer {...props} className={props.className} ref={componentRef}>
-            {steps}
+            {children && children.map((step, index) => {
+                return React.cloneElement(step, {
+                    ...step.props,
+                    index,
+                    active: props.activeStep === index
+                });
+            })}
         </StepperContainer>
-    );
-});
+    )
+);
 
 Stepper.propTypes = {
     /** Dois ou mais componentes `<Step>` */
-    children: Proptypes.node,
+    children: Proptypes.node.isRequired,
 
    /** Define o passo ativo (baseado no index zero). */
    activeStep: Proptypes.number,
