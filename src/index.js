2640689c5aaf1332ab6f6f1b251ed32b62b18e7d
Ernesto Resende <ernestoresende@hotmail.com>
2021-04-14 08:14:37 -0300

    chore: removed unused index files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Button from "./components/Button";
-import Avatar from "./components/Avatar";
-import ThemeProvider from "./components/ThemeProvider";
-
-export { Button };
-export { Avatar };
-export { ThemeProvider };
