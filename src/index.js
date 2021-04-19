5b5f0bf7dffded2eae22b8e2b0531bdc488b7931
Ernesto Resende <ernestoresende@hotmail.com>
2021-04-19 16:29:59 -0300

    stack: added all previously built components to index.js

---
+++ src/index.js
@@ -0,0 +1,44 @@
+export { default as ThemeProvider } from "./components/ThemeProvider";
+export * from "./components/ThemeProvider";
+
+export { default as Accordion } from "./components/Accordion";
+export * from "./components/Accordion";
+
+export { default as AccordionItem } from "./components/AccordionItem";
+export * from "./components/AccordionItem";
+
+export { default as Avatar } from "./components/Avatar";
+export * from "./components/Avatar";
+
+export { default as Button } from "./components/Button";
+export * from "./components/Button";
+
+export { default as CurrencyLabel } from "./components/CurrencyLabel";
+export * from "./components/CurrencyLabel";
+
+export { default as PaymentMethodItem } from "./components/PaymentMethodItem";
+export * from "./components/PaymentMethodItem";
+
+export { default as PaymentPrompt } from "./components/PaymentPrompt";
+export * from "./components/PaymentPrompt";
+
+export { default as PaymentStatus } from "./components/PaymentStatus";
+export * from "./components/PaymentStatus";
+
+export { default as SavedCardBadge } from "./components/SavedCardBadge";
+export * from "./components/SavedCardBadge";
+
+export { default as Select } from "./components/Select";
+export * from "./components/Select";
+
+export { default as Step } from "./components/Step";
+export * from "./components/Step";
+
+export { default as Stepper } from "./components/Stepper";
+export * from "./components/Stepper";
+
+export { default as TextField } from "./components/TextField";
+export * from "./components/TextField";
+
+export { default as Tooltip } from "./components/Tooltip";
+export * from "./components/Tooltip";
