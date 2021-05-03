01829fce7f464bf11fe23abb0d8e10ad117eac7d
ernestoresende <ernestoresende@hotmail.com>
2021-05-03 14:38:37 -0300

    chore: removed unused vars

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Proptypes from "prop-types";
 import { uniqueId } from "lodash";
