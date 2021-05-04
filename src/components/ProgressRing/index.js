f9ea0dbd47e2b7f39b30c754c5b818fa02a333f7
ernestoresende <ernestoresende@hotmail.com>
2021-05-04 11:58:17 -0300

    feat: added ProgressRing atom component

---
+++ src/components/ProgressRing/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Proptypes from "prop-types";
+import styled from "styled-components";
+
+import * as V from "../../styles/variables";
+
+const StyledCircle = styled.circle`
+    transform: rotate(-90deg);
+    transform-origin: 50% 50%;
+`;
+const StyledText = styled.text`
+    font-family: ${V.FontFaces.Poppins};
+    font-weight: 700;
+    
+`;
+
+const ProgressRing = props => {
+    const normalizedRadius = props.radius - props.stroke * 2;
+    const circumference = normalizedRadius * 2 * Math.PI;
+
+    const strokeDashOffset = circumference - (props.progress / 100) * circumference;
+
+    return (
+        <svg height={props.radius * 2} width={props.radius * 2}>
+            <StyledCircle
+                stroke={props.strokeColor}
+                fill="transparent"
+                strokeWidth={props.stroke}
+                strokeDasharray={circumference + " " + circumference}
+                strokeDashoffset={strokeDashOffset}
+                r={normalizedRadius}
+                cx={props.radius}
+                cy={props.radius}
+            />
+            {props.text ? (
+                <StyledText 
+                  text-anchor="middle"
+                  alignment-baseline="central" 
+                  fill={props.textColor}
+                  fontSize={props.textFontSize}
+                  x={props.textXAxis} 
+                  y={props.textYAxis}
+                >
+                    {props.text}
+                </StyledText>
+            ) : null}
+        </svg>
+    );
+};
+
+ProgressRing.propTypes = {
+    /** Define o tamanho do raio do anel. */
+    radius: Proptypes.number.isRequired,
+
+    /** Define a largura do traçado do anel. */
+    stroke: Proptypes.number.isRequired,
+
+    /** Define a cor do traçado do anel. */
+    strokeColor: Proptypes.string.isRequired,
+
+    /** Caso especificada, define a string de texto que deve estar dentro do anél de progresso. */
+    text: Proptypes.string,
+    
+    /** Caso especificada, define a cor do texto dentro do anél de progresso. */
+    textColor: Proptypes.string,
+
+    /** Caso seja especificado, define o tamanho da fonte do texto dentro do anel de progresso (em px). */
+    textFontSize: Proptypes.number,
+
+    textXAxis: Proptypes.number,
+
+    textYAxis: Proptypes.number,
+
+    /** Define o número do progresso em uma escala de 0 a 100. */
+    progress: Proptypes.number.isRequired,
+};
+
+export default ProgressRing;
