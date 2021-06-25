250f43392310729449574b23b717f3ff514243e4
ernestoresende <ernestoresende@hotmail.com>
2021-06-25 11:50:59 -0300

    refactor: removed unused mdx-embed instances

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|mdx)"],
     addons: [
-        "storybook-addon-mdx-embed",
         "@storybook/addon-docs",
         "@storybook/addon-a11y",
         "@storybook/addon-essentials",
