68467c901f0f3fd7ac11007bc0c71786f7e05124
Ernesto Resende <ernestoresende@hotmail.com>
2021-07-13 19:34:06 -0300

    refactor: require fonts from consumer project instead of injecting on globalStyles

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-import { fontFaces } from "./typography";
 
 const GlobalStyles = createGlobalStyle`
-  ${fontFaces}
-
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
