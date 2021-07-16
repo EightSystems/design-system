4cae807d504072c1e5d60957eff95ff24e5ec226
Ernesto Resende <ernestoresende@hotmail.com>
2021-07-16 16:21:09 -0300

    chore: started react-native implementation

---
+++ base/src/components/Button/index.native.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const Button = () => {
+  return (
+    <View style={{ width: 100, height: 100, backgroundColor: 'blue' }}>
+      <Text>Alô</Text>
+    </View>
+  )
+}
+
+export default Button
