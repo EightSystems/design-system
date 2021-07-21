5b2f9523c7fc8cb614695c8d6e8e63523b4cbe27
Ernesto Resende <ernestoresende@hotmail.com>
2021-07-21 14:44:29 -0300

    chore: moved native-docs package

---
+++ packages/docs/src/markdown/MdxContent.js
@@ -0,0 +1,26 @@
+// Use your MDX content with this component.
+
+import React from "react";
+const MDX = require("@mdx-js/runtime");
+const { MDXProvider } = require("@mdx-js/react");
+import components from "./components";
+
+const mdxComponents = {
+    ...components,
+};
+
+// Add variables here if needed.
+const scope = {};
+
+// Children is an MDX string
+const MdxContent = ({ children, style = {} }) => {
+    return (
+        <View style={style}>
+            <MDXProvider components={mdxComponents} scope={scope}>
+                <MDX>{children}</MDX>
+            </MDXProvider>
+        </View>
+    );
+};
+
+export default MdxContent;
---
+++ packages/docs/src/views/HomeScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Theme from "../../../core/src/styles/theme";
+import styled from "styled-components";
+import { FlatList } from "react-native";
+import StoryList from "../../StoryImports";
+
+const ItemLabel = styled.Text`
+    color: ${Theme.colors.primaryTint};
+    font-size: 16px;
+`;
+const ItemView = styled.Pressable`
+    padding: 10px;
+    background: ${Theme.colors.background};
+`;
+
+const Item = ({ title, navigation }) => (
+    <ItemView onPress={() => navigation.navigate("Details", { indexName: title })}>
+        <ItemLabel>{title}</ItemLabel>
+    </ItemView>
+);
+
+const HomeScreen = ({ navigation }) => {
+    const StoryKeys = Object.keys(StoryList);
+
+    const renderItem = ({ item }) => <Item title={item} navigation={navigation} />;
+
+    return <FlatList data={StoryKeys} renderItem={renderItem} keyExtractor={item => item} />;
+};
+
+export default HomeScreen;
