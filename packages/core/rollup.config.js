a65736bb00ddd2113b9107bab3bdd402db47b228
Ernesto Resende <ernestoresende@hotmail.com>
2021-07-23 14:24:17 -0300

    chore: cleaning directory structure, setting up the bundler

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,3 +1,55 @@
+// import typescript from "rollup-plugin-typescript2";
+// import commonjs from "@rollup/plugin-commonjs";
+// import external from "rollup-plugin-peer-deps-external";
+// import postcss from "rollup-plugin-postcss";
+// import resolve from "@rollup/plugin-node-resolve";
+// import url from "@rollup/plugin-url";
+// import svgr from "@svgr/rollup";
+// import json from "@rollup/plugin-json";
+// import multiInput from "rollup-plugin-multi-input";
+//
+// export default {
+//     input: ["src/**/*.js"],
+//     output: [
+//         {
+//             dir: "dist",
+//             format: "cjs",
+//             exports: "named",
+//             sourcemap: true,
+//             preserveModules: false,
+//             preserveModulesRoot: "src",
+//         },
+//     ],
+//     plugins: [
+//         multiInput(),
+//         external(),
+//         postcss({
+//             modules: true,
+//         }),
+//         url(),
+//         svgr({
+//             svgoConfig: {
+//                 plugins: {
+//                     removeViewBox: false,
+//                 },
+//             },
+//         }),
+//         json(),
+//         resolve(),
+//         typescript({
+//             tsconfig: "./tsconfig.json",
+//             abortOnError: false,
+//             clean: true,
+//             include: ["*.ts+(|x)", "**/*.ts+(|x)", "*.js+(|x)", "**/*.js+(|x)"],
+//             exclude: ["node_modules/**"],
+//         }),
+//         commonjs(),
+//     ],
+// };
+//
+// Gonna test a new configuration pattern that allows me to produce sepeare bundles for native and non-native components
+//
+
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
@@ -8,41 +60,89 @@ import svgr from "@svgr/rollup";
 import json from "@rollup/plugin-json";
 import multiInput from "rollup-plugin-multi-input";
 
-export default {
-    input: ["src/**/*.js"],
+const webInput = "./src/web/index.js";
+const nativeInput = "./src/native/index.js";
+
+const outputOptions = {
+    format: "cjs",
+    exports: "named",
+    sourcemap: true,
+    preserveModules: false,
+    preserveModulesRoot: "src",
+};
+
+const defaultPlugins = [
+    multiInput(),
+    external(),
+    postcss({
+        modules: true,
+    }),
+    url(),
+    svgr({
+        svgoConfig: {
+            plugins: {
+                removeViewBox: false,
+            },
+        },
+    }),
+    json(),
+    resolve(),
+];
+
+const webConfig = {
+    input: webInput,
     output: [
         {
             dir: "dist",
-            format: "cjs",
-            exports: "named",
-            sourcemap: true,
-            preserveModules: false,
-            preserveModulesRoot: "src",
+            ...outputOptions,
         },
+        ,
     ],
     plugins: [
-        multiInput(),
-        external(),
-        postcss({
-            modules: true,
-        }),
-        url(),
-        svgr({
-            svgoConfig: {
-                plugins: {
-                    removeViewBox: false,
-                },
-            },
-        }),
-        json(),
-        resolve(),
         typescript({
             tsconfig: "./tsconfig.json",
             abortOnError: false,
             clean: true,
             include: ["*.ts+(|x)", "**/*.ts+(|x)", "*.js+(|x)", "**/*.js+(|x)"],
             exclude: ["node_modules/**"],
+            tsconfigOverride: {
+                compilerOptions: {
+                    jsx: "react",
+                },
+                include: ["./src/assets", "./src/web", "./src/hooks", "./src/styles", "./src/utils"],
+            },
         }),
         commonjs(),
+        ...defaultPlugins,
     ],
 };
+
+const nativeConfig = {
+    input: nativeInput,
+    output: [
+        {
+            dir: "mobile",
+            ...outputOptions,
+        },
+    ],
+    plugins: [
+        typescript({
+            tsconfig: "./tsconfig.json",
+            abortOnError: false,
+            clean: true,
+            include: ["*.ts+(|x)", "**/*.ts+(|x)", "*.js+(|x)", "**/*.js+(|x)"],
+            exclude: ["node_modules/**"],
+            tsconfigOverride: {
+                compilerOptions: {
+                    module: "ES2015",
+                    jsx: "react",
+                },
+                include: ["./src/assets", "./src/native", "./src/hooks", "./src/styles", "./src/utils"],
+            },
+        }),
+        commonjs(),
+        ...defaultPlugins,
+    ],
+};
+
+export default [webConfig, nativeConfig];
