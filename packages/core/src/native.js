1f01f881e94cc353dcf00bd171e0e4dea3bb84fa
Ernesto Resende <ernestoresende@hotmail.com>
2021-08-02 04:54:57 -0300

    refactor: import/export schema for input files

---
+++ packages/core/src/native.js
@@ -0,0 +1,5 @@
+import Theme from "./styles/theme";
+import ThemeProviderInjector from "./components/GlobalInjector/index.native";
+import Button from "./components/Button/index.native";
+
+export { ThemeProviderInjector, Theme, Button };
