a489239fad1f1d08907c3032a0548750b38d0a79
Ernesto Resende <ernestoresende@hotmail.com>
2021-08-06 12:14:22 -0300

    refactor(tsx): useExtendedTheme hook

--- packages/core/src/hooks/useExtendedTheme.tsx
+++ packages/core/src/hooks/useExtendedTheme.tsx
@@ -1,13 +1,7 @@
 import { theme, nativeTheme } from "../theme";
 import { merge } from "lodash";
 
-/**
- * Merges the default theme with a custom set of objects to compose a different
- * @param {string} environment Determinates if the default theme should be the web or nativeTheme. Defaults to "web"
- * @param {object} overrides Objects to be overriden/extended
- * @returns {object} Returns the result of the merge between the default theme and the override object
- */
-export const useExtendedTheme = (environment, overrides) => {
+export const useExtendedTheme = (environment: string, overrides: object) => {
     const defaultTheme = environment === "web" || undefined ? theme : nativeTheme;
     const themeMerge = merge(defaultTheme, overrides || {});
 
