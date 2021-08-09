88ea87ce2cf03882737b4d7bb811d19eb78b276b
Ernesto Resende <ernestoresende@hotmail.com>
2021-08-09 19:30:13 -0300

    chore: updated native imports entrypoint

--- packages/core/src/native.js
+++ packages/core/src/native.js
@@ -3,5 +3,6 @@ import ThemeProviderInjector from "./components/core/ThemeProviderInjector/index
 
 import Button from "./components/primitives/Button/index.native";
 import Modal from "./components/composites/Modal/index.native";
+import TextField from "./components/primitives/TextField/index.native";
 
-export { ThemeProviderInjector, Theme, Button, Modal };
+export { ThemeProviderInjector, Theme, Button, Modal, TextField };
