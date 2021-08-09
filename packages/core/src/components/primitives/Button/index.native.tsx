40f527dbb1d42519f065868752edf848d77c0e13
Ernesto Resende <ernestoresende@hotmail.com>
2021-08-09 06:22:52 -0300

    fix(Button): moved type declaration to avoid faulty generation of declaration files

--- packages/core/src/components/primitives/Button/index.native.tsx
+++ packages/core/src/components/primitives/Button/index.native.tsx
@@ -1,26 +1,51 @@
-import React, { isValidElement, forwardRef } from "react";
-import { ActivityIndicator } from "react-native";
+import React, { isValidElement } from "react";
+import { ActivityIndicator, PressableProps } from "react-native";
 
-import { ButtonProps } from "./types";
 import Spacer from "../Spacer/index.native";
 import * as S from "./styled.native";
 
-const Button = (
-    {
-        disabled = false,
-        color = "primary",
-        loading = false,
-        variant = "solid",
-        size = "medium",
-        startIcon,
-        endIcon,
-        iconPadding,
-        spinnerColor,
-        children,
-        ...props
-    }: ButtonProps,
-    ref: any
-) => {
+export type ButtonProps = PressableProps & {
+    /** The color of the button background, or the button outline when the `variant` is "outline". */
+    color?: string;
+
+    /** Determinates the button variant the be used. */
+    variant?: "solid" | "outline" | "ghost";
+
+    /** When `true`, the button will be shown as disabled and with a spinner. */
+    loading?: boolean;
+
+    /** The size of the button. */
+    size?: "small" | "medium" | "large";
+
+    /** The startIcon to use in the Button. */
+    startIcon?: React.ReactNode;
+
+    /** The end icon to use in the Button. */
+    endIcon?: React.ReactNode;
+
+    /** The padding between the icon and the text. */
+    iconPadding?: number;
+
+    /** The color of the spinner. */
+    spinnerColor?: any;
+
+    /** When `true` the button is shown as disabled. */
+    disabled?: boolean;
+};
+
+const Button = ({
+    disabled = false,
+    color = "primary",
+    loading = false,
+    variant = "solid",
+    size = "medium",
+    startIcon,
+    endIcon,
+    iconPadding,
+    spinnerColor,
+    children,
+    ...props
+}: ButtonProps) => {
     return (
         <S.ButtonPressable
             {...props}
@@ -29,7 +54,6 @@ const Button = (
             data-variant={variant}
             data-size={size}
             data-disabled={disabled}
-            ref={ref}
         >
             {loading ? (
                 <React.Fragment>
@@ -45,7 +69,7 @@ const Button = (
                 : null}
             {startIcon && isValidElement(startIcon) ? <Spacer size={iconPadding ? iconPadding : 0} /> : null}
 
-            <S.StyledText data-size={size} data-color={color}>
+            <S.StyledText data-size={size} data-color={color} data-variant={variant}>
                 {children}
             </S.StyledText>
 
@@ -59,4 +83,4 @@ const Button = (
     );
 };
 
-export default forwardRef(Button);
+export default Button;
