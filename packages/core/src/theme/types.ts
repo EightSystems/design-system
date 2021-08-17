e6c995442d9e7093789c1ae1b2a5c9aa2c41a7e1
Ernesto Resende <ernestoresende@hotmail.com>
2021-08-17 19:29:14 -0300

    chore: added type definitions for theme objects

---
+++ packages/core/src/theme/types.ts
@@ -0,0 +1,45 @@
+export type ShadowTypes = "xxs" | "xs" | "sm" | "default" | "md" | "lg" | "xlg" | "xxlg";
+
+export type RadiusTypes = "none" | "sm" | "md" | "lg" | "xl" | "pill" | "full";
+
+export type BorderTypes = "sm" | "default" | "lg";
+
+export type SpacingTypes = "xxs" | "xs" | "sm" | "default" | "md" | "lg" | "xlg" | "xxlg";
+
+export type LetterSpacingTypes = "xxs" | "xs" | "sm" | "default" | "md" | "lg" | "xlg";
+
+export type LineHeightsTypes = "none" | "shorther" | "short" | "short" | "base" | "tall" | "taller";
+
+export type ColorTypes =
+    | "primary"
+    | "primaryShade"
+    | "primaryTint"
+    | "primaryContrast"
+    | "secondary"
+    | "secondaryShade"
+    | "secondaryTint"
+    | "secondaryContrast"
+    | "tertiary"
+    | "tertiaryShade"
+    | "tertiaryTint"
+    | "tertiaryContrast"
+    | "warning"
+    | "warningShade"
+    | "warningTint"
+    | "warningContrast"
+    | "danger"
+    | "dangerShade"
+    | "dangerTint"
+    | "dangerContrast"
+    | "dark"
+    | "darkShade"
+    | "darkTint"
+    | "darkContrast"
+    | "medium"
+    | "mediumShade"
+    | "mediumTint"
+    | "mediumContrast"
+    | "light"
+    | "lightTint"
+    | "lightShade"
+    | "lightContrast";
