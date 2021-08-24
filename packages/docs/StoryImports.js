93e43c495c602484869715a1ab14e5785204eb5b
Vin Souza <vin@8sistemas.com>
2021-08-24 19:05:25 -0300

    chore: fix button stories and docs native app

--- packages/docs/StoryImports.js
+++ packages/docs/StoryImports.js
@@ -1,9 +1,11 @@
 import IntroductionStory from "../core/src/stories/0_Introduction.stories";
 import ButtonStory from "../core/src/stories/Button.stories";
+import ProgressRingStory from "../core/src/stories/ProgressRing.stories";
 
 const storyList = {
     Introduction: IntroductionStory,
     Button: ButtonStory,
+    ProgressRing: ProgressRingStory,
 };
 
 export default storyList;
