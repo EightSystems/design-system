a070bf488952d8fbfd72944e140b29413bbe8edc
Vin Souza <vin@8sistemas.com>
2021-08-24 20:28:45 -0300

    chore: fix image loading and ensure text is being used for string children

--- packages/docs/StoryImports.js
+++ packages/docs/StoryImports.js
@@ -1,9 +1,13 @@
 import IntroductionStory from "../core/src/stories/0_Introduction.stories";
+import UsageStory from "../core/src/stories/1_Usage.stories";
+import ThemingStory from "../core/src/stories/2_Theming.stories";
 import ButtonStory from "../core/src/stories/Button.stories";
 import ProgressRingStory from "../core/src/stories/ProgressRing.stories";
 
 const storyList = {
     Introduction: IntroductionStory,
+    Usage: UsageStory,
+    Theming: ThemingStory,
     Button: ButtonStory,
     ProgressRing: ProgressRingStory,
 };
