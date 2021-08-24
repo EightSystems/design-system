7c245b09a5b178ea8f64cfec66ebfe16f796e7bc
Vin Souza <vin@8sistemas.com>
2021-08-24 20:31:57 -0300

    chore: add more stories to the import

--- packages/docs/StoryImports.js
+++ packages/docs/StoryImports.js
@@ -1,6 +1,8 @@
 import IntroductionStory from "../core/src/stories/0_Introduction.stories";
 import UsageStory from "../core/src/stories/1_Usage.stories";
 import ThemingStory from "../core/src/stories/2_Theming.stories";
+//import AccordionStory from "../core/src/stories/Accordion.stories";
+//import AvatarStory from "../core/src/stories/Avatar.stories";
 import ButtonStory from "../core/src/stories/Button.stories";
 import ProgressRingStory from "../core/src/stories/ProgressRing.stories";
 
@@ -8,6 +10,8 @@ const storyList = {
     Introduction: IntroductionStory,
     Usage: UsageStory,
     Theming: ThemingStory,
+    //Accordion: AccordionStory,
+    //Avatar: AvatarStory,
     Button: ButtonStory,
     ProgressRing: ProgressRingStory,
 };
