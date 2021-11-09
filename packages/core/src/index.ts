1633137dc65373f999c9c8348a28f02fed36288a
Vin Souza <vin@8sistemas.com>
2021-11-09 13:31:30 -0300

    chore: add font icon

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,49 +1,41 @@
 // Core components
-import { theme as Theme } from "./theme";
-import ThemeProviderInjector from "./components/core/ThemeProviderInjector";
-
-// Layout Components
-import Box from "./components/layout/Box";
-import Spacer from "./components/layout/Spacer";
-import { Col, Row, RowProps, ColProps } from "./components/layout/Grid";
-
-// Typography Components
-import { Heading, WebHeadingProps } from "./components/typography/Heading";
-import { Text, WebTextProps } from "./components/typography/Text";
-
-// Composite components
-import { PaymentStatus, WebPaymentStatusProps } from "./components/composites/PaymentStatus";
 import Accordion from "./components/composites/Accordion";
 import AccordionItem from "./components/composites/AccordionItem";
 import { Modal, WebModalProps } from "./components/composites/Modal";
-import PaymentMethodItem from "./components/composites/PaymentMethodItem";
-import PaymentPrompt from "./components/composites/PaymentMethodItem";
+import { default as PaymentMethodItem, default as PaymentPrompt } from "./components/composites/PaymentMethodItem";
+// Composite components
+import { PaymentStatus, WebPaymentStatusProps } from "./components/composites/PaymentStatus";
 import SavedCardBadge from "./components/composites/SavedCardBadge";
-
+import ThemeProviderInjector from "./components/core/ThemeProviderInjector";
+import { ProgressRing, WebProgressRingProps } from "./components/feedback/ProgressRing";
 // Feedback components
 import { Spinner, WebSpinnerProps } from "./components/feedback/Spinner";
 import { Stepper, StepperProps } from "./components/feedback/Stepper";
-import { Tooltip, WebTooltipProps } from "./components/feedback/Tooltip";
 import Toast from "./components/feedback/Toast";
-
+import { Tooltip, WebTooltipProps } from "./components/feedback/Tooltip";
 // Form components
 import { Button, WebButtonProps } from "./components/forms/Button";
-import { TextField, WebTextFieldProps } from "./components/forms/TextField";
 import Select from "./components/forms/Select";
-
+import { TextField, WebTextFieldProps } from "./components/forms/TextField";
+// Layout Components
+import Box from "./components/layout/Box";
+import { Col, ColProps, Row, RowProps } from "./components/layout/Grid";
+import Spacer from "./components/layout/Spacer";
 // Primitve components
 import { Avatar, WebAvatarProps } from "./components/primitives/Avatar";
 import { CurrencyLabel } from "./components/primitives/CurrencyLabel";
 import { Divider, WebDividerProps } from "./components/primitives/Divider";
 import { Icon, WebIconProps } from "./components/primitives/Icon";
-import { ProgressRing, WebProgressRingProps } from "./components/feedback/ProgressRing";
 import { Skeleton, WebSkeletonProps } from "./components/primitives/Skeleton";
-
+// Typography Components
+import { Heading, WebHeadingProps } from "./components/typography/Heading";
+import { Text, WebTextProps } from "./components/typography/Text";
 // Hooks
 import { useContainerDimensions } from "./hooks/useContainerDimensions";
 import { useDetectOutsideClick } from "./hooks/useDetectOutsideClick";
 import { useExtendedTheme } from "./hooks/useExtendedTheme";
-import { withMediaWatcher, media, mediaMinWidth } from "./theme/media";
+import { theme as Theme } from "./theme";
+import { media, mediaMinWidth, withMediaWatcher } from "./theme/media";
 
 export {
     Accordion,
@@ -81,7 +73,6 @@ export {
     media as createMediaMaxWidthQuery,
     mediaMinWidth as createMediaMinWidthQuery,
 };
-
 export type {
     ColProps,
     WebAvatarProps,
