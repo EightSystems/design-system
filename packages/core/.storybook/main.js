ec4c6ae6c4d39a306f7db5fda48e49415229f6db
Vin Souza <vin@8sistemas.com>
2021-11-27 15:47:45 -0300

    chore: storybook upgrade and assets

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -1,5 +1,11 @@
 module.exports = {
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|mdx)"],
+    staticDirs: [
+        {
+            from: "../src/components/primitives/WebVideo/static/",
+            to: "/assets/webvideo/",
+        },
+    ],
     addons: [
         "@storybook/addon-docs",
         "@storybook/addon-a11y",
