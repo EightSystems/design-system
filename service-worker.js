ede4aafb2218c9944336860510c70786436aad77
tryvin <vin@8sistemas.com>
2022-04-26 18:06:55 +0000

    Deploying to gh-pages from @ EightSystems/design-system@7f9cbca1633627bb99dbe21c1e609cd51ef2d38b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c9aec38f03df97b8c144d29cfea81338.js"
+  "precache-manifest.c1c3753f768e1bb49cc796b62c56b95c.js"
 );
 
 self.addEventListener('message', (event) => {
